1132037c8f0dbd63568cb1597b49d8b3cdfcbb7a
MertCan Çobanoğlu <121344042+Mertcancobanoglu@users.noreply.github.com>
2023-04-05 22:43:11 +0300

    Java

---
+++ 1.gün/methods/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args)
+    {
+    String mesaj ="Bugün hava çok güzel";
+    String yeniMesaj =sehirVer();
+    System.out.println(yeniMesaj);
+    int sayi = topla(5,7);
+        System.out.println(sayi);
+    public static void ekle(){
+
+    }
+        public static void sil(){
+
+    }
+        public static void güncelle(){
+
+    }
+        public static void topla(int sayi1,int sayi2){
+        return 5;
+    }
+        public static void sehirVer(){
+
+    }
+    }
+}
