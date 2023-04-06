26f0871fbd9c5ae802f720844e6000b480b30eaa
MertCan Çobanoğlu <121344042+Mertcancobanoglu@users.noreply.github.com>
2023-04-06 23:36:58 +0300

    JavaCamp-2

---
+++ 2.Gün/classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,10 @@
+public class ProductManager {
+    public void Add(Product product){
+        //JBC
+        System.out.println();
+    }
+
+    public void Add2(int id,String name,String description,int stockAmount,double pricei,String yeniOzellik){
+
+    }
+}
---
+++ 2.Gün/overriding/src/OgrenciKredisiManager.java
@@ -0,0 +1,5 @@
+public class OgrenciKredisiManager extends BaseKrediManager {
+    public double hesapla(double tutar){
+        return tutar*1.10;
+    }
+}
